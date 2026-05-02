c7cec2aece536913a6f06aa1be372c3ad901d0f2
pedromeireles23 <pemeirels@gmail.com>
2026-05-02 16:16:45 -0300

    Problema 03 - Achar os fatores primos de um número natural

---
+++ ProjectEuler/Exercicios/Problem3.cs
@@ -0,0 +1,32 @@
+public class Problem3
+{
+//   Os fatores primos de 13195 são 5, 7, 13 e 29.
+
+// Qual é o maior fator primo do número dado?
+
+public static void FatoresPrimos(int num)
+  {
+    List <int> fatores = new List<int>();
+
+
+    while(num % 2 == 0)
+    {
+      fatores.Add(2);
+      num /= 2;
+
+    }
+    for (int i = 3; i * i <= num; i += 2)
+        {
+            while (num % i == 0)
+            {
+                fatores.Add(i);
+                num /= i;
+            }
+        }
+
+    if(num > 2) fatores.Add(num);
+
+
+    System.Console.WriteLine($"Os fatores primos de {num} são: " +string.Join(",",  fatores));
+
+}}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -1,2 +1,3 @@
 ﻿// Console.WriteLine(Problem1.MultiploDe3Ou5(10));
-Problem2.SequenciaFibonacci(10);
+// Problem2.SequenciaFibonacci(10);
+Problem3.FatoresPrimos(10);
